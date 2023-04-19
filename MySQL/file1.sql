eacff5b7a24e7a18ba6cd581d82f0bf40f19a9aa
Nikhil Sharma <nikhilsharmanikk53@gmail.com>
2023-04-19 11:01:04 +0530

    Add files via upload

---
+++ MySQL/file1.sql
@@ -0,0 +1,5 @@
+insert into users values
+(1,"Nikhil","nikhilsharmanikk53@gmail.com","115084bbo","9518147923" , "Haryana" , "2001-01-10", "M",1 ),
+(2,"Parry","parryelis@gmail.com","768084bbo","678247923" , "punjab" , "2002-01-10", "M",0 ),
+(3,"Sara","tendous@gmail.com","929084bbo","999981423" , "delhi" , "2003-01-10", "f",1 ),
+(4,"Cook","cooktim@gmail.com","282084bbo","89718147923" , "austrilia" , "2004-01-10", "M",0 )
