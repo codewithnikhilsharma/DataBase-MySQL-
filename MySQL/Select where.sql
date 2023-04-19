83c8f3b8da00bed9d6cba2f4c6b828e13c4ee117
Nikhil Sharma <nikhilsharmanikk53@gmail.com>
2023-04-19 11:29:22 +0530

    Update Select where.sql

--- MySQL/Select where.sql
+++ MySQL/Select where.sql
@@ -2,3 +2,9 @@ select * from users where id < 2;
 select * from users where id > 2;
 select * from users where id = 2;
 select * from users where id != 2;
+
+// to show only 
+select id,name,age FROM users
+
+// use As
+SELECT id, name AS "Student name" , dob FROM users
